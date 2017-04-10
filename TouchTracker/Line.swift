ef1358e7452d0b7cd6f0b90eebd7041a5a91845e
Sean Melnick Kelly <seanmelnickkelly@Seans-MacBook-Pro.local>
2017-04-10 13:39:31 -0400

    Finished up to pg. 322, app takes in straight pointed lines created by the user's touch and then adds them onto an array to hold the lines

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Sean Melnick Kelly on 4/10/17.
+//  Copyright © 2017 Sean Melnick Kelly. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
